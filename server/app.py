1af03b71afe5f2258766bc9afc5573e120ee6360
Password Manager <1105465401@qq.com>
2026-05-25 15:56:36 +0800

    优化界面样式，聚焦重庆特色
    - 将小程序更名为'重庆周边游'
    - 更新路线数据为重庆及周边特色路线（10条路线）
    - 优化UI设计，采用重庆红色主题
    - 更新筛选条件为：火锅美食、山城徒步、景区观光
    - 更新所有页面样式，融入重庆特色元素
    - 更新README文档，详细介绍重庆特色路线
    - 修改项目配置，体现重庆主题

--- server/app.py
+++ server/app.py
@@ -7,154 +7,250 @@ import random
 app = Flask(__name__)
 CORS(app)
 
-# 路线数据
+# 重庆及周边路线数据
 ROUTES_DATA = [
     {
         "id": 1,
-        "name": "北京经典一日游",
+        "name": "重庆市区经典一日游",
         "type": "scenic",
         "duration": "1day",
-        "distance": "25公里",
-        "rating": 4.8,
-        "reviews": 128,
-        "description": "探索北京经典景点，感受古都文化",
-        "image": "/images/route1.jpg",
-        "highlights": ["故宫", "天安门广场", "景山公园"],
-        "food": ["北京烤鸭", "炸酱面", "豆汁儿"],
+        "distance": "20公里",
+        "rating": 4.9,
+        "reviews": 356,
+        "description": "探索山城重庆，感受8D魔幻都市魅力",
+        "image": "/images/chongqing-city.jpg",
+        "highlights": ["解放碑", "洪崖洞", "长江索道", "李子坝轻轨穿楼"],
+        "food": ["重庆火锅", "小面", "酸辣粉"],
         "spots": [
-            {"name": "故宫博物院", "time": "2-3小时", "ticket": "60元"},
-            {"name": "天安门广场", "time": "1小时", "ticket": "免费"},
-            {"name": "景山公园", "time": "1小时", "ticket": "2元"}
+            {"name": "解放碑步行街", "time": "1-2小时", "ticket": "免费"},
+            {"name": "洪崖洞民俗风貌区", "time": "2-3小时", "ticket": "免费"},
+            {"name": "长江索道", "time": "1小时", "ticket": "单程20元"},
+            {"name": "李子坝轻轨穿楼", "time": "30分钟", "ticket": "免费"}
         ],
         "foodSpots": [
-            {"name": "全聚德烤鸭店", "type": "北京烤鸭", "price": "人均150元"},
-            {"name": "老北京炸酱面馆", "type": "炸酱面", "price": "人均30元"}
+            {"name": "珮姐老火锅", "type": "九宫格火锅", "price": "人均120元"},
+            {"name": "花市豌杂面", "type": "重庆小面", "price": "人均15元"}
         ],
-        "tags": ["文化", "历史", "经典"],
-        "location": {"lat": 39.915, "lng": 116.397}
+        "tags": ["市区", "经典", "8D魔幻", "网红打卡"],
+        "location": {"lat": 29.563, "lng": 106.551}
     },
     {
         "id": 2,
-        "name": "胡同美食探索之旅",
+        "name": "重庆火锅美食两日游",
         "type": "food",
-        "duration": "1day",
-        "distance": "8公里",
-        "rating": 4.6,
-        "reviews": 89,
-        "description": "深入北京胡同，品尝地道小吃",
-        "image": "/images/route2.jpg",
-        "highlights": ["南锣鼓巷", "烟袋斜街", "什刹海"],
-        "food": ["卤煮火烧", "炒肝", "糖葫芦"],
+        "duration": "2day",
+        "distance": "15公里",
+        "rating": 4.8,
+        "reviews": 289,
+        "description": "品尝地道重庆火锅，体验麻辣鲜香的美食文化",
+        "image": "/images/hotpot.jpg",
+        "highlights": ["火锅博物馆", "火锅一条街", "夜市小吃"],
+        "food": ["九宫格火锅", "毛肚火锅", "串串香", "江湖菜"],
         "spots": [
-            {"name": "南锣鼓巷", "time": "2小时", "ticket": "免费"},
-            {"name": "烟袋斜街", "time": "1小时", "ticket": "免费"},
-            {"name": "什刹海", "time": "2小时", "ticket": "免费"}
+            {"name": "重庆火锅博物馆", "time": "1-2小时", "ticket": "免费"},
+            {"name": "南滨路美食街", "time": "2-3小时", "ticket": "免费"},
+            {"name": "观音桥好吃街", "time": "2小时", "ticket": "免费"}
         ],
         "foodSpots": [
-            {"name": "姚记炒肝店", "type": "炒肝", "price": "人均20元"},
-            {"name": "卤煮火烧店", "type": "卤煮火烧", "price": "人均25元"}
+            {"name": "周师兄大刀腰片火锅", "type": "腰片火锅", "price": "人均130元"},
+            {"name": "楠火锅", "type": "复古火锅", "price": "人均100元"},
+            {"name": "胡记蹄花汤", "type": "蹄花汤", "price": "人均40元"}
         ],
-        "tags": ["美食", "胡同", "小吃"],
-        "location": {"lat": 39.937, "lng": 116.403}
+        "tags": ["火锅", "美食", "两日游", "地道小吃"],
+        "location": {"lat": 29.557, "lng": 106.573}
     },
     {
         "id": 3,
-        "name": "长城徒步一日游",
+        "name": "南山一棵树徒步观景",
         "type": "hiking",
         "duration": "1day",
-        "distance": "60公里",
-        "rating": 4.9,
-        "reviews": 256,
-        "description": "攀登长城，感受历史的厚重",
-        "image": "/images/route3.jpg",
-        "highlights": ["慕田峪长城", "箭扣长城"],
-        "food": ["农家菜", "山野菜"],
+        "distance": "8公里",
+        "rating": 4.7,
+        "reviews": 178,
+        "description": "徒步南山，俯瞰重庆全景，欣赏最美夜景",
+        "image": "/images/nanshan.jpg",
+        "highlights": ["南山一棵树", "南山植物园", "老君洞"],
+        "food": ["南山泉水鸡", "农家菜"],
         "spots": [
-            {"name": "慕田峪长城", "time": "4-5小时", "ticket": "40元"},
-            {"name": "箭扣长城", "time": "3-4小时", "ticket": "免费"}
+            {"name": "南山一棵树观景台", "time": "2小时", "ticket": "30元"},
+            {"name": "南山植物园", "time": "2-3小时", "ticket": "免费"},
+            {"name": "老君洞", "time": "1小时", "ticket": "免费"}
         ],
         "foodSpots": [
-            {"name": "长城脚下农家院", "type": "农家菜", "price": "人均60元"}
+            {"name": "南山泉水鸡一条街", "type": "泉水鸡", "price": "人均80元"}
         ],
-        "tags": ["徒步", "长城", "户外"],
-        "location": {"lat": 40.431, "lng": 116.570}
+        "tags": ["徒步", "夜景", "自然", "观景"],
+        "location": {"lat": 29.533, "lng": 106.596}
     },
     {
         "id": 4,
-        "name": "京郊两日休闲游",
+        "name": "武隆天生三桥一日游",
         "type": "scenic",
-        "duration": "2day",
-        "distance": "120公里",
-        "rating": 4.7,
-        "reviews": 67,
-        "description": "逃离城市喧嚣，享受京郊宁静",
-        "image": "/images/route4.jpg",
-        "highlights": ["古北水镇", "司马台长城", "密云水库"],
-        "food": ["水库鱼", "农家菜", "豆腐宴"],
+        "duration": "1day",
+        "distance": "170公里",
+        "rating": 4.9,
+        "reviews": 412,
+        "description": "探索《变形金刚4》取景地，感受大自然的鬼斧神工",
+        "image": "/images/wulong.jpg",
+        "highlights": ["天生三桥", "龙水峡地缝", "仙女山"],
+        "food": ["武隆碗碗羊肉", "烤全羊"],
         "spots": [
-            {"name": "古北水镇", "time": "4-5小时", "ticket": "140元"},
-            {"name": "司马台长城", "time": "2-3小时", "ticket": "40元"},
-            {"name": "密云水库", "time": "2小时", "ticket": "免费"}
+            {"name": "天生三桥", "time": "3-4小时", "ticket": "95元"},
+            {"name": "龙水峡地缝", "time": "2小时", "ticket": "85元"},
+            {"name": "仙女山国家森林公园", "time": "2-3小时", "ticket": "60元"}
         ],
         "foodSpots": [
-            {"name": "古北水镇餐厅", "type": "特色菜", "price": "人均80元"},
-            {"name": "水库鱼馆", "type": "水库鱼", "price": "人均100元"}
+            {"name": "武隆碗碗羊肉", "type": "碗碗羊肉", "price": "人均50元"}
         ],
-        "tags": ["两日游", "休闲", "京郊"],
-        "location": {"lat": 40.658, "lng": 117.266}
+        "tags": ["自然风光", "电影取景地", "世界遗产"],
+        "location": {"lat": 29.428, "lng": 107.760}
     },
     {
         "id": 5,
-        "name": "奥林匹克公园徒步",
-        "type": "hiking",
+        "name": "大足石刻文化一日游",
+        "type": "scenic",
         "duration": "1day",
-        "distance": "15公里",
-        "rating": 4.5,
-        "reviews": 45,
-        "description": "现代建筑与自然景观的完美结合",
-        "image": "/images/route5.jpg",
-        "highlights": ["鸟巢", "水立方", "奥林匹克森林公园"],
-        "food": ["园区餐厅", "快餐"],
+        "distance": "110公里",
+        "rating": 4.8,
+        "reviews": 267,
+        "description": "参观世界文化遗产，感受千年佛教艺术",
+        "image": "/images/dazu.jpg",
+        "highlights": ["宝顶山石刻", "北山石刻", "大足石刻博物馆"],
+        "food": ["大足邮亭鲫鱼", "冬菜尖"],
         "spots": [
-            {"name": "鸟巢", "time": "1-2小时", "ticket": "50元"},
-            {"name": "水立方", "time": "1小时", "ticket": "30元"},
-            {"name": "奥林匹克森林公园", "time": "2-3小时", "ticket": "免费"}
+            {"name": "宝顶山石刻", "time": "3-4小时", "ticket": "115元"},
+            {"name": "北山石刻", "time": "2小时", "ticket": "70元"},
+            {"name": "大足石刻博物馆", "time": "1小时", "ticket": "免费"}
         ],
         "foodSpots": [
-            {"name": "鸟巢餐厅", "type": "快餐", "price": "人均40元"}
+            {"name": "邮亭鲫鱼", "type": "邮亭鲫鱼", "price": "人均60元"}
         ],
-        "tags": ["现代", "公园", "休闲"],
-        "location": {"lat": 39.993, "lng": 116.396}
+        "tags": ["文化", "世界遗产", "佛教艺术"],
+        "location": {"lat": 29.707, "lng": 105.722}
     },
     {
         "id": 6,
-        "name": "上海外滩夜景游",
-        "type": "scenic",
+        "name": "磁器口古镇怀旧之旅",
+        "type": "food",
         "duration": "1day",
         "distance": "10公里",
-        "rating": 4.7,
-        "reviews": 189,
-        "description": "欣赏外滩夜景，感受上海魅力",
-        "image": "/images/route6.jpg",
-        "highlights": ["外滩", "南京路", "陆家嘴"],
-        "food": ["生煎包", "小笼包"],
+        "rating": 4.6,
+        "reviews": 234,
+        "description": "漫步千年古镇，品尝地道小吃，感受老重庆风情",
+        "image": "/images/ciqikou.jpg",
+        "highlights": ["磁器口古镇", "宝轮寺", "码头文化"],
+        "food": ["陈麻花", "古镇鸡杂", "毛血旺"],
         "spots": [
-            {"name": "外滩", "time": "2小时", "ticket": "免费"},
-            {"name": "南京路步行街", "time": "2小时", "ticket": "免费"},
-            {"name": "陆家嘴", "time": "1小时", "ticket": "免费"}
+            {"name": "磁器口古镇", "time": "3-4小时", "ticket": "免费"},
+            {"name": "宝轮寺", "time": "1小时", "ticket": "免费"},
+            {"name": "码头遗址", "time": "30分钟", "ticket": "免费"}
         ],
         "foodSpots": [
-            {"name": "南翔馒头店", "type": "小笼包", "price": "人均50元"}
+            {"name": "陈麻花总店", "type": "陈麻花", "price": "人均20元"},
+            {"name": "古镇鸡杂", "type": "鸡杂", "price": "人均40元"}
         ],
-        "tags": ["夜景", "城市", "经典"],
-        "location": {"lat": 31.235, "lng": 121.490}
+        "tags": ["古镇", "小吃", "怀旧", "文化"],
+        "location": {"lat": 29.585, "lng": 106.453}
+    },
+    {
+        "id": 7,
+        "name": "金佛山滑雪温泉两日游",
+        "type": "hiking",
+        "duration": "2day",
+        "distance": "130公里",
+        "rating": 4.7,
+        "reviews": 156,
+        "description": "冬季滑雪泡温泉，夏季避暑赏杜鹃",
+        "image": "/images/jinfoshan.jpg",
+        "highlights": ["金佛山滑雪场", "天星温泉", "古佛洞"],
+        "food": ["方竹笋", "烤全羊"],
+        "spots": [
+            {"name": "金佛山滑雪场", "time": "4-5小时", "ticket": "滑雪票200元"},
+            {"name": "天星国际温泉", "time": "2-3小时", "ticket": "128元"},
+            {"name": "古佛洞", "time": "1小时", "ticket": "40元"}
+        ],
+        "foodSpots": [
+            {"name": "金佛山农家乐", "type": "方竹笋宴", "price": "人均80元"}
+        ],
+        "tags": ["滑雪", "温泉", "避暑", "两日游"],
+        "location": {"lat": 29.058, "lng": 107.183}
+    },
+    {
+        "id": 8,
+        "name": "酉阳桃花源两日游",
+        "type": "scenic",
+        "duration": "2day",
+        "distance": "340公里",
+        "rating": 4.8,
+        "reviews": 198,
+        "description": "探寻陶渊明笔下的世外桃源，感受土家族风情",
+        "image": "/images/youyang.jpg",
+        "highlights": ["桃花源景区", "龚滩古镇", "乌江画廊"],
+        "food": ["土家菜", "乌江鱼"],
+        "spots": [
+            {"name": "酉阳桃花源", "time": "3-4小时", "ticket": "100元"},
+            {"name": "龚滩古镇", "time": "2-3小时", "ticket": "免费"},
+            {"name": "乌江画廊游船", "time": "2小时", "ticket": "120元"}
+        ],
+        "foodSpots": [
+            {"name": "龚滩古镇土家菜", "type": "土家菜", "price": "人均60元"}
+        ],
+        "tags": ["世外桃源", "古镇", "土家族", "两日游"],
+        "location": {"lat": 28.839, "lng": 108.773}
+    },
+    {
+        "id": 9,
+        "name": "白帝城瞿塘峡一日游",
+        "type": "scenic",
+        "duration": "1day",
+        "distance": "400公里",
+        "rating": 4.9,
+        "reviews": 312,
+        "description": "朝辞白帝彩云间，感受三峡壮美风光",
+        "image": "/images/baidicheng.jpg",
+        "highlights": ["白帝城", "瞿塘峡", "夔门"],
+        "food": ["奉节脐橙", "三峡鱼"],
+        "spots": [
+            {"name": "白帝城景区", "time": "2-3小时", "ticket": "100元"},
+            {"name": "瞿塘峡游船", "time": "2小时", "ticket": "150元"},
+            {"name": "夔门观景台", "time": "1小时", "ticket": "免费"}
+        ],
+        "foodSpots": [
+            {"name": "奉节脐橙采摘", "type": "脐橙", "price": "人均30元"}
+        ],
+        "tags": ["三峡", "历史文化", "诗词", "游船"],
+        "location": {"lat": 31.017, "lng": 109.464}
+    },
+    {
+        "id": 10,
+        "name": "解放碑夜景美食之旅",
+        "type": "food",
+        "duration": "1day",
+        "distance": "5公里",
+        "rating": 4.7,
+        "reviews": 245,
+        "description": "夜游解放碑，品尝地道夜市小吃",
+        "image": "/images/jiefangbei-night.jpg",
+        "highlights": ["解放碑夜景", "八一好吃街", "较场口夜市"],
+        "food": ["烧烤", "串串", "冰粉", "凉虾"],
+        "spots": [
+            {"name": "解放碑步行街", "time": "1-2小时", "ticket": "免费"},
+            {"name": "八一好吃街", "time": "2小时", "ticket": "免费"},
+            {"name": "较场口夜市", "time": "2小时", "ticket": "免费"}
+        ],
+        "foodSpots": [
+            {"name": "好又来酸辣粉", "type": "酸辣粉", "price": "人均10元"},
+            {"name": "山城小汤圆", "type": "小汤圆", "price": "人均8元"}
+        ],
+        "tags": ["夜景", "夜市", "小吃", "市区"],
+        "location": {"lat": 29.557, "lng": 106.580}
     }
 ]
 
 @app.route('/')
 def index():
     return jsonify({
-        "message": "周边游助手API服务",
+        "message": "重庆周边游API服务",
         "version": "1.0.0",
         "total_routes": len(ROUTES_DATA),
         "endpoints": [
@@ -291,5 +387,5 @@ def get_stats():
     })
 
 if __name__ == '__main__':
-    print(f"加载路线数据: {len(ROUTES_DATA)}条路线")
+    print(f"加载重庆周边游路线数据: {len(ROUTES_DATA)}条路线")
     app.run(host='0.0.0.0', port=9091, debug=True)
